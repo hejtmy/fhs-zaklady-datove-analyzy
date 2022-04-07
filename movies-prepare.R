e02780c14c72201afef83c297c7595fc7ba5a177
Lukáš Hejtmánek <hejtmy@gmail.com>
2022-04-08 00:05:02 +0200

    Adds demo

--- movies-prepare.R
+++ movies-prepare.R
@@ -1,29 +1,6 @@
 library(tidyverse)
 
-load_movies_metadata <- function(relative_pth, nrows=0){
-  pth <- file.path(relative_pth, "data", "movies", "movies_metadata.csv")
-  df_movies <- read.csv(pth)
-  set.seed(666)
-  df_movies <- df_movies %>%
-    #mutate(vote_count = ifelse(vote_count == 0), NA, vote_count) %>%
-    select(-c(overview, homepage, belongs_to_collection, production_companies,
-              production_countries, tagline, poster_path)) %>%
-    mutate(adult = adult == "True",
-           budget = as.numeric(budget)/10^6,
-           revenue = revenue/10^6,
-           id = as.integer(id)) %>%
-    separate(release_date, c("year", "month", "day"), sep="-", convert = TRUE) %>%
-    mutate(is_comedy = grepl("Comedy", genres),
-           is_action = grepl("Action", genres),
-           is_horror = grepl("Horror", genres),
-           is_family = grepl("Family", genres),
-           is_drama = grepl("Drama", genres),
-           is_thriller = grepl("Thriller", genres))
-  if(is.numeric(sample)){
-    df_movies <- sample_n(df_movies, sample)
-  }
-  return(df_movies)
-}
+source("functions/loading.R")
 
 df_movies <- load_movies_metadata(".")
 df_movies <- df_movies %>%
@@ -37,4 +14,5 @@ df_ratings <- df_ratings %>%
   left_join(select(df_movies, id, title, month, is_comedy, is_action, is_horror, vote_average, original_language),
             by=c("movieId"="id")) %>%
   filter(!is.na(title))
-write_csv(df_ratings, "movies_ratings.csv")
+
+write_csv(df_ratings, "data/movies/movies_ratings.csv")
